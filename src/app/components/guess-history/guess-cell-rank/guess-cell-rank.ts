a4c74c7752ecf27fbb167b77caa2068460156331
Klimm-Max <klimm.max@gmail.com>
2026-01-05 19:06:15 +0100

    implementing a small component for displaying the rank

---
+++ src/app/components/guess-history/guess-cell-rank/guess-cell-rank.ts
@@ -0,0 +1,23 @@
+import { Component, computed, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-guess-cell-rank',
+  imports: [],
+  templateUrl: './guess-cell-rank.html',
+  styleUrl: './guess-cell-rank.css',
+})
+export class GuessCellRank {
+  @Input({ required: true }) value!: number;
+  @Input({ required: true }) targetValue!: number;
+
+  readonly isCorrect = computed(() => this.value === this.targetValue);
+
+  readonly direction = computed<'up' | 'down' | null>(() => {
+    if (this.isCorrect()) return null;
+    return this.targetValue > this.value ? 'up' : 'down';
+  });
+
+  readonly isWithinRange = computed<boolean>(() => {
+    return Math.abs(this.value - this.targetValue) <= 20
+  });
+}
--- src/app/components/guess-history/guess-history.ts
+++ src/app/components/guess-history/guess-history.ts
@@ -5,10 +5,11 @@ import { GuessCellArtwork } from './guess-cell-artwork/guess-cell-artwork';
 import { GuessCellColors } from "./guess-cell-colors/guess-cell-colors";
 import { GuessCellCmc } from "./guess-cell-cmc/guess-cell-cmc";
 import { GuessCellTags } from "./guess-cell-tags/guess-cell-tags";
+import { GuessCellRank } from "./guess-cell-rank/guess-cell-rank";
 
 @Component({
   selector: 'app-guess-history',
-  imports: [GuessCellArtwork, GuessCellColors, GuessCellCmc, GuessCellTags],
+  imports: [GuessCellArtwork, GuessCellColors, GuessCellCmc, GuessCellTags, GuessCellRank],
   templateUrl: './guess-history.html',
   styleUrl: './guess-history.css',
 })
