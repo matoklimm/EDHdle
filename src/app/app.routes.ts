e847326b20026b782fc7777b5d0a119164100092
Klimm-Max <klimm.max@gmail.com>
2026-01-05 07:47:20 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: Home,
+    },
+    {
+        path: '**',
+        component: Home,
+    },
+];
